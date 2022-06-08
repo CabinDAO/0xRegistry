e4d4b44e89a68b2796ae71220a76f5d8ace5a6d4
Matthew Watman <mattwatman@gmail.com>
2022-06-08 14:13:59 -0400

    Update registry contract, factory deploy script and hardhat config

--- packages/contracts/scripts/deploy_factory.ts
+++ packages/contracts/scripts/deploy_factory.ts
@@ -1,14 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    const registryAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-    const registry = await ethers.getContractAt("TcrRegistry", registryAddress);
+    const registryAddress = "0xd321949Ec9e8dE5c60323FcD247Ce2Ec8eB64eEE";
 
     const Factory = await ethers.getContractFactory("TcrFactory");
     const factory = await Factory.deploy(registryAddress);
     
     await factory.deployed();
-    await registry.transferOwnership(factory.address);
 
     console.log("TCR Factory deployed to:", factory.address);
 }
